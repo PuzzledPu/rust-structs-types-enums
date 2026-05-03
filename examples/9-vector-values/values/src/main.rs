d2fde333d0a473d60e136c60bff47217b8f0572c
PuzzledPu <puzzledpu@gmail.com>
2026-05-03 20:04:23 +0000

    Practice lab: summing vector and pattern matching.

--- examples/9-vector-values/values/src/main.rs
+++ examples/9-vector-values/values/src/main.rs
@@ -10,21 +10,27 @@ fn get_item(index: usize) {
     println!("The value at index {} is {:?}", index, value);
 }
 
+fn sum_vector(vec: Vec<i32>) -> i32 {
+    vec.iter().sum()
+}
+
 fn main() {
     let vec = vec![1, 2, 3, 4, 5];
     get_item(3);
 
     // Retrieve a value at a specific index
     let third_value = vec[2];
-    //println!("The third value in the vector is: {}", third_value);
+    println!("The third value in the vector is: {}", third_value);
 
     // Retrieve the last value
     let last_value = vec.last().unwrap();
     //println!("The last value in the vector is: {}", last_value);
 
     // Retrieve the first value using pattern matching
-    // match vec.first() {
-    //     Some(first_value) => println!("The first value in the vector is: {}", first_value),
-    //     None => println!("The vector is empty!"),
-    // }
+    match vec.first() {
+        Some(first_value) => println!("The first value in the vector is: {}", first_value),
+        None => println!("The vector is empty!"),
+    }
+
+    println!("The sum of the vector is: {}", sum_vector(vec));
 }
