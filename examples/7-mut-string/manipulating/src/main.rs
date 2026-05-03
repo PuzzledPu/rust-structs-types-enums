312a7471afdd1143ab631e23d73d0b77a27e2736
PuzzledPu <puzzledpu@gmail.com>
2026-05-03 19:46:17 +0000

    Practice lab: fn for getting longest word

--- examples/7-mut-string/manipulating/src/main.rs
+++ examples/7-mut-string/manipulating/src/main.rs
@@ -1,25 +1,38 @@
+fn longest_word(sentence: &str) -> &str {
+    let mut longest = "";
+    for word in sentence.split_whitespace() {
+        if word.len() > longest.len() {
+            longest = word;
+        }
+    }
+    longest
+}
+
+
 fn main() {
     let sentence = "the quick brown fox jumps over the lazy dog".to_string();
     // Use slicing to get the first three characters of the sentence
-    //println!("{}", &sentence[0..=4]);
+    println!("{}", &sentence[0..=4]);
 
     // concatenate using format!
     let description = format!("Title: Quick story\n{}", sentence);
-    //println!("{}", description);
+    println!("{}", description);
 
     // iterate over the characters in the sentence
-    // for c in sentence.chars() {
-    //     match c {
-    //         'a' | 'e' | 'i' | 'o' | 'u' => println!("Got a vowel!"),
-    //         _ => continue,
-    //     }
-    // }
+    for c in sentence.chars() {
+        match c {
+            'a' | 'e' | 'i' | 'o' | 'u' => println!("Got a vowel!"),
+            _ => continue,
+        }
+    }
 
     // Split and collect into a vector
     //let words: Vec<&str> = sentence.split_whitespace().collect();
-    let words = sentence.split(' ').collect::<Vec<&str>>();
+    // let words = sentence.split(' ').collect::<Vec<&str>>();
     //println!("{:?}", words);
 
     let reversed = sentence.chars().rev().collect::<String>();
     println!("{}", reversed);
+
+    println!("Longest word: {}", longest_word(&sentence));
 }
