63de537225287dac988da581d657ff27d7659215
PuzzledPu <puzzledpu@gmail.com>
2026-05-03 15:22:44 +0000

    Practice lab: add fn to struct with impl

--- examples/1-defining-structs/defining/src/main.rs
+++ examples/1-defining-structs/defining/src/main.rs
@@ -4,12 +4,25 @@ struct Person {
     first_name: String,
     last_name: String,
     age: u8,
+    weight: Option<usize>
+}
+
+impl Person {
+    fn full_name(&self) -> String {
+        format!("{} {}", self.first_name, self.last_name)
+    }
 }
 
 fn main() {
-    println!("{:?}", Person {
+
+    let person =  Person {
         first_name: "John".to_string(),
         last_name: "Doe".to_string(),
         age: 25,
-    });
+        weight: Some(76)
+    };
+
+    println!("{:?}", person);
+    println!("Full name: {}", person.full_name());
+
 }
