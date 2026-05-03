6bcb1f6563e8babb1083000ceff991b8cf33bd6c
PuzzledPu <puzzledpu@gmail.com>
2026-05-03 20:37:57 +0000

    Practice lab: fns for adding ends to vector and combine vectors

--- examples/10-vector-elements/elements/src/main.rs
+++ examples/10-vector-elements/elements/src/main.rs
@@ -1,3 +1,14 @@
+fn add_to_ends(v: &mut Vec<i32>, element: i32) -> &Vec<i32> {
+    v.insert(0, element); // Add to the beginning
+    v.push(element);
+    v // Add to the end
+}
+
+fn combine_vectors(v1: &mut Vec<i32>, v2: Vec<i32>) -> &Vec<i32> {
+    v1.extend(v2);
+    v1 // Add all elements of v2 to v1
+}
+
 fn main() {
     let mut v = vec![1, 2, 3];
     v.push(4);
@@ -16,4 +27,7 @@ fn main() {
     // insert items at a given index
     v.insert(0, 0);
     println!("{:?}", v); // Output: [0, 1, 2, 3, 4, 5, 6, 7, 8] 
+
+    println!("{:?}", add_to_ends(&mut v, 42));
+    println!("{:?}", combine_vectors(&mut v, vec![71, 97]));
 }
